f9ca36c337033a94a7db34b650ba1bfa90947d7e
王嵩 <wangsong@wangsongdeMacBook-Pro.local>
2014-12-10 10:55:11 +0800

    123

--- iyours/AppDelegate.m
+++ iyours/AppDelegate.m
@@ -18,6 +18,9 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
     NSLog(@"test");
+    
+    
+    UIButton *btn = [[UIButton alloc] init];
     return YES;
 }
 
